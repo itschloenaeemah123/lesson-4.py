d1d94c42eec5b86ee42404facfe0680776df0157
itschloenaeemah123 <owodoyin321@yahoo.com>
2025-02-15 18:34:17 +0100

    lesson 4.py

---
+++ count_note.py
@@ -0,0 +1,9 @@
+amount = int(input("Enter an amount to withdraw: "))
+
+note1 = amount//100
+note2 = (amount%100)//50
+note3 = ((amount%100)%50)//10
+
+print("Notes of 100: ", note1)
+print("Notes of 50: ", note2)
+print("Notes of 10: ", note3)
