f34e520c6c44a9319ef8374eede3b9d633356d01
itschloenaeemah123 <owodoyin321@yahoo.com>
2025-02-15 18:22:20 +0100

    lesson4.py

---
+++ cal_Average.py
@@ -0,0 +1,13 @@
+#Assigning values
+tree1 = 98
+tree2 = 94
+tree3 = 41
+tree4 = 75
+tree5 = 21
+
+sum = tree1+tree2+tree3+tree4+tree5
+print ("sum of Heights:",sum)
+
+avg = sum/5
+print("Average Height is: ",avg)
+
